76d9885d206b27d5fc2d676dd00c8b5cb067f167
三叶 <rzh466470@outlook.com>
2022-08-09 15:20:38 +0800

    bug

--- src/views/WorkOrder/operate.vue
+++ src/views/WorkOrder/operate.vue
@@ -7,25 +7,18 @@
         <search ref="search" title="工单搜索" :model.sync="searchFrom.taskCode" />
         <DropDown ref="DropDown" title="工单状态" :model.sync="searchFrom.status" :work="WorkOrderStatus" />
         <lsButton @click="JobSearch" />
-      </el-form></div>
+      </el-form>
+    </div>
     <div>
       <!-- 添加按钮 -->
-      <lsButton
-        icon="el-icon-circle-plus-outline"
-        color="addBtn"
-        title="新建"
-        @click="AddWorkOrder"
-      />
-      <lsButton title="工单配置" color="config" />
-      <el-table
-        :data="WorkOrderList"
-        style="width: 100%"
-        :lazy="true"
-        empty-text="暂无数据"
-        type="index"
-      >
-        <tableColumn title="操作" label="查看详情">查看详情</tableColumn>
-        <tableColumn title="序号" type="index" :pag="Pag.pageIndex" />
+      <lsButton icon="el-icon-circle-plus-outline" color="addBtn" title="新建" @click="AddWorkOrder" />
+      <lsButton title="工单配置" color="config" @click="GetWorkOrderAlert" />
+      <el-table :data="WorkOrderList" style="width: 100%" :lazy="true" empty-text="暂无数据" type="index">
+        <el-table-column
+          label="操作"
+        >
+          <template slot-scope="scope"><el-button type="text" @click="ViewDetails(scope)">查看详情</el-button></template></el-table-column>
+        <tableColumn title="序号" type="index" :pag=" Pag.pageIndex" />
         <tableColumn title="工单编号" label="taskCode" />
         <tableColumn title="设备编号" label="innerCode" />
         <tableColumn title="工单类型" label="taskType.typeName" />
@@ -36,50 +29,23 @@
       </el-table>
     </div>
     <div class="dataBtn">
-      <div class="Data">
-        共{{ Pag.totalCount }}记录 第{{ Pag.pageIndex }}/{{ Pag.totalPage }}页
-      </div>
-      <lsButton
-        ref="btn"
-        title="上一页"
-        :disable="disable"
-        color="pag"
-        @click="PreviousPage"
-      />
-      <lsButton
-        title="下一页"
-        :disable="disable1"
-        color="pag"
-        @click="NextPage"
-      />
+      <div class="Data">共{{ Pag.totalCount }}记录  第{{ Pag.pageIndex }}/{{ Pag.totalPage }}页</div>
+      <lsButton ref="btn" title="上一页" :disable="disable" color="pag" @click="PreviousPage" />
+      <lsButton title="下一页" :disable="disable1" color="pag" @click="NextPage" />
     </div>
     <!--  新增弹窗 -->
     <Dialogue title="添加工单" :visible.sync="visible">
-      <el-form
-        ref="form"
-        class="el-input"
-        label-width="80px"
-        label-position="right"
-      >
-        <!-- 设备编号 -->
-        <search ref="search" title="设备编号" />
-        <!-- 工单类型 -->
-        <DropDown ref="DropDown" :work="WorkOrderTypeList" title="工单类型" />
-        <el-form-item label="补货数量">
-          <span class="BackOrder">
-            <i class="el-icon-document" /> <span>补货清单</span></span>
-        </el-form-item>
-        <!-- 运营人员 -->
-        <DropDown ref="DropDown" :work="WorkOrderStatus" title="运营人员" />
-        <!-- 留言框 -->
-        <!-- <el-form-item> -->
-        <MessageBox />
-        <!-- </el-form-item> -->
-        <div class="addWorkBtn">
-          <lsButton title="取消" color="config" @click="CancelWork" />
-          <lsButton title="确定" color="addBtn" />
-        </div>
-      </el-form>
+      <BulletFrame :list="WorkOrderTypeList" />
+    </Dialogue>
+    <!-- 工单配置 -->
+    <Dialogue title="工单配置" :visible.sync="visible1">
+      <div class="Replenishment">
+        <span>补货预警值：</span>  <el-input-number v-model="num" controls-position="right" />
+      </div>
+      <div class="addWorkBtn">
+        <lsButton title="取消" color="config" @click="visible1=false" />
+        <lsButton title="确定" color="addBtn" @click="SetUpAutomaticReplenishmentWarn" />
+      </div>
     </Dialogue>
   </div>
 </template>
@@ -89,11 +55,7 @@ import BulletFrame from './components/BulletFrame.vue'
 import search from '@/components/search'
 import DropDown from '@/components/DropDown'
 import lsButton from '@/components/ls-button'
-import {
-  getWorkOrderStatus,
-  getWorkOrderList,
-  getWorkOrderType
-} from '@/api/WorkOrder'
+import { getWorkOrderStatus, getWorkOrderList, getWorkOrderType, GetWorkOrderAlert, SetUpAutomaticReplenishmentWarn } from '@/api/WorkOrder'
 import moment from 'moment'
 import tableColumn from '@/components/tablecolumn'
 import Dialogue from '@/components/Dialogue'
@@ -118,8 +80,8 @@ export default {
       status: '',
       taskCode: '',
       moment,
-      searchFrom: {
-        // 搜索表单数据
+      num: 1, // 工单预警数据
+      searchFrom: { // 搜索表单数据
         status: '',
         taskCode: ''
       },
@@ -138,7 +100,9 @@ export default {
     this.getWorkOrderType()
   },
   // 生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
+  mounted() {
+
+  },
   methods: {
     // 获取工单列表
     async getWorkOrderList() {
@@ -147,18 +111,14 @@ export default {
         ...this.searchFrom,
         isRepair: false
       })
-      // console.log(data);
-      const workList = await this.ProcessingWorkOrderStatus(
-        data.currentPageRecords
-      )
+      const workList = await this.ProcessingWorkOrderStatus(data.currentPageRecords)
       this.WorkOrderList = workList
       this.Pag = data
     },
     // 获取工单状态
     async getWorkOrderStatus() {
-      const { data } = await getWorkOrderStatus();
-      this.WorkOrderStatus = data;
-      console.log(data);
+      const { data } = await getWorkOrderStatus()
+      this.WorkOrderStatus = data
     },
     // 上一页
     NextPage() {
@@ -184,13 +144,9 @@ export default {
     },
     // 处理工单状态
     ProcessingWorkOrderStatus(data) {
-      data.forEach((ele) => {
-        ele.createType === 0
-          ? (ele.createType = '自动')
-          : (ele.createType = '手动')
-        ele.updateTime = this.moment(ele.updateTime)
-          .utcOffset(8)
-          .format('YYYY.MM.DD HH:mm:ss')
+      data.forEach(ele => {
+        ele.createType === 0 ? ele.createType = '自动' : ele.createType = '手动'
+        ele.updateTime = this.moment(ele.updateTime).utcOffset(8).format('YYYY.MM.DD HH:mm:ss')
       })
       return data
     },
@@ -229,24 +185,24 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-.WorkOrder {
+.WorkOrder{
   width: 100%;
   height: 60px;
   background-color: #fff;
   display: flex;
-  padding: 10px 20px;
+  padding: 10px 20px ;
   margin: 20px;
   border-radius: 10px;
 }
-.Data {
+.Data{
   width: 100%;
   height: 100%;
   background-color: #fff;
   // line-height: 100px;
   padding-left: 30px;
-  color: #dbdfe5;
+  color:#dbdfe5;
 }
-.dataBtn {
+.dataBtn{
   display: flex;
   height: 40px;
   margin-bottom: 30px;
@@ -254,32 +210,46 @@ export default {
   align-items: center;
   margin-right: 30px;
 }
-.ViewDetails {
-  color: #409eff;
-  font-style: normal;
+.ViewDetails{
+    color: #409eff;
+    font-style: normal;
+
 }
-.el-form-item {
+.el-form-item{
   margin-bottom: 0;
 }
-.form-group {
+.form-group{
   width: 100%;
 }
-.el-input {
+.el-input{
   width: 100% !important;
 }
-.title-from {
+.title-from{
   display: flex;
 }
-.el-form-item {
+.el-form-item{
   margin-bottom: 10px;
 }
-.BackOrder {
-  color: #4368e1;
+.BackOrder{
+  color:#4368e1;
   cursor: pointer;
 }
-.addWorkBtn {
+.addWorkBtn{
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ margin-top: 30px;
+}
+.el-form-item__content{
   display: flex;
-  justify-content: center;
+}
+.Replenishment{
+  display: flex;
   align-items: center;
+  padding: 0 60px;
+span{
+  display: inline-block;
+  width: 100px;
+}
 }
 </style>
